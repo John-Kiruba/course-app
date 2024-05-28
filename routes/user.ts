caf757ecfb5e8bf75c35cd9fa9208fb15467674d
John Kiruba I <100715617+John-Kiruba@users.noreply.github.com>
2024-05-28 22:38:23 +0530

    Added user signin and course purchase logic implementing JWT AUTH

--- routes/user.ts
+++ routes/user.ts
@@ -1,12 +1,19 @@
 import { Router, Request, Response } from "express";
+import { JWT_SECRET as jwt_secret } from "../index";
+
 const jwt = require("jsonwebtoken");
+
 const userMiddleware = require("../middleware/user");
+
 const { User } = require("../db/index");
 
 const router = Router();
 
 export interface AuthData extends Express.Request {
   username?: string;
+  params: {
+    courseId?: string;
+  };
 }
 
 router.get("/", (_req: Request, res: Response) => {
@@ -30,11 +37,61 @@ router.post("/signup", async (req: Request, res: Response) => {
   });
 });
 
-//usercourses puchase logic
-router.post("/courses/:courseId", userMiddleware, async (req: AuthData) => {
-  const username = req.username;
-  console.log(username);
+router.post("/signin", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log(username, password);
+
+  const isValidated = await User.findOne({
+    name: username,
+    password,
+  });
+
+  if (isValidated) {
+    const token = jwt.sign(
+      {
+        username,
+      },
+      jwt_secret
+    );
+
+    res.json({ token });
+  } else {
+    res.status(411).json({
+      message: "Incorrect Credentials",
+    });
+  }
 });
+
+//usercourses puchase logic
+router.post(
+  "/courses/:courseId",
+  userMiddleware,
+  async (req: AuthData, res: Response) => {
+    const username = req.username;
+
+    const courseId = req.params.courseId;
+
+    try {
+      const updatePurchase = await User.updateOne(
+        { name: username },
+        {
+          $addToSet: {
+            purchasedCourses: courseId,
+          },
+        }
+      );
+
+      if (updatePurchase.nModified > 0) {
+        res.status(200).send("Course Purchased Successfully");
+      } else {
+        res.status(500).send("Error Purchasing Course");
+      }
+    } catch (e) {
+      res.status(500).send(`error: ${e}`);
+    }
+  }
+);
 
 //user purchased courses fetch
 router.get(
